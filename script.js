fac25168fa98977af865acbc62b734f87e56bc81
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2026-05-17 15:59:50 -0400

    tweak mobile resize

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// resize vh for mobile
+mobileResize();
+
+function mobileResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', () => {
+    mobileResize();
+});
+
 let longitude;
 let latitude;
 let sunriseTime;
